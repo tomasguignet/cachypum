1a1d464b1afc5edb61b841ab6acb1faa3c985ef5
tomasguignet <tomasguignet9@gmail.com>
2025-05-11 22:27:15 -0300

    5 commit

--- front/src/pages/MoreInfo/MoreInfo.jsx
+++ front/src/pages/MoreInfo/MoreInfo.jsx
@@ -1,38 +1,41 @@
-import React from "react";
+import caballoNegro from "../../images/caballoNegro.jpg";
+import chicaMate from "../../images/chicaMate.jpeg";
+import grupoPersonas from "../../images/grupoPersonas.jpeg";
+import caballoAbajo from "../../images/caballoAbajo.jpg";
 
 const MoreInfo = () => {
   return (
-    <section className="">
-      <p>
-        En nuestra fundación, diseñamos experiencias significativas para
-        empresas que desean fortalecer su compromiso con la Responsabilidad
-        Social Empresarial (RSE).
+    <section className="bg-oliveGreen brightness-110 text-center text-white pt-3.5">
+      <p className="text-sm px-4 mb-4">
+        En nuestra <strong>fundación</strong>, diseñamos experiencias
+        significativas para empresas que desean fortalecer su compromiso con la
+        <strong> Responsabilidad Social Empresarial</strong> (RSE).
       </p>
-      <div>
-        <div>
-          <img src="" alt="" />
-          <h4>EVENTOS RESPONSABLES</h4>
-          <p>
+      <div className="font-[Sora] px-1 pb-4">
+        <div className="mb-4">
+          <img src={caballoNegro} alt="" />
+          <h4 className="font-black text-sm mt-1">EVENTOS RESPONSABLES</h4>
+          <p className="font-light text-xs">
             A través de nuestros Eventos Responsables, ofrecemos celebraciones
             de fin de año con impacto positivo, jornadas de forestación y
             cuidado ambiental. Actividades de bienestar para colaboradores y sus
             familias.
           </p>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h4>SOLUCIONES DE ASE</h4>
-          <p>
+        <div className="mb-4">
+          <img src={chicaMate} alt="" />
+          <h4 className="font-black text-sm mt-1">SOLUCIONES DE ASE</h4>
+          <p className="font-light text-xs">
             Convertimos regalos corporativos en una oportunidad para generar
             impacto social. Nuestros regalos empresariales son creados con
             dedicación por personas en situación de vulnerabilidad, brindándoles
             una oportunidad de inclusión y desarrollo.
           </p>
         </div>
-        <div>
-          <img src="" alt="" />
-          <h4>TEAM BULDING</h4>
-          <p>
+        <div className="mb-4">
+          <img src={grupoPersonas} alt="" />
+          <h4 className="font-black text-sm mt-1">TEAM BULDING</h4>
+          <p className="font-light text-xs">
             A través de dinámicas al aire libre, experiencias con animales y
             propuestas innovadoras, creamos jornadas que potencian la cohesión y
             el bienestar de los equipos. ¡Transformá la manera de conectar con
@@ -40,9 +43,9 @@ const MoreInfo = () => {
           </p>
         </div>
         <div>
-          <img src="" alt="" />
-          <h4>COLABORA CON PROYECTOS</h4>
-          <p>
+          <img src={caballoAbajo} alt="" />
+          <h4 className="font-black text-sm mt-1">COLABORA CON PROYECTOS</h4>
+          <p className="font-light text-xs">
             Consulta y conoce nuestros proyectos activos para ser parte de la
             construccion de un tejido social mas solido en Mendoza.
           </p>
